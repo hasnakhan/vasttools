a4d42c226a27b02525499bf42582ee778dbdaa6c
hasnakhan <hasna.khan@mail.utoronto.ca>
2021-10-04 10:38:24 -0400

    scinet scripts

---
+++ 20210924_vast_align_combine_scinet2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00
+#SBATCH --job-name=20210924_vast_align_combine
+#SBATCH --output=20210924_vast_align_combine_output.txt
+#SBATCH --mail-user=hasna.khan@mail.utoronto.ca
+#SBATCH --mail-type=ALL
+
+export PATH=/scratch/n/nprovart/khanha55/vast-tools:$PATH #using scratch path here since home is read-only and align will write to vast-out
+export PATH=/home/n/nprovart/khanha55/bowtie-1.3.1-linux-x86_64:$PATH
+
+module load NiaEnv/2019b #added this and the next 2 lines for attempt 2, after attempt 1 failed for lack of python (??)
+module load intel/2019u4
+module load python/3.8.5
+module load perl/5.34.0
+module load gcc/9.2.0
+module load r/4.0.3
+
+cd $SLURM_SUBMIT_DIR
+
+for file in /scratch/n/nprovart/khanha55/R1_for_later/*.gz; do
+vast-tools align $file -sp araTha10 --expr --IR_version 2 --dbDir /scratch/n/nprovart/khanha55/vast-tools -c 80
+done
+
+vast-tools combine -sp araTha10 -o vast_out --dbDir /scratch/n/nprovart/khanha55/vast-tools
